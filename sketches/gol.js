6107381a4b713780c1ba7f2d3335cd8af0f7c6e9
fettycini <37485201+fettycini@users.noreply.github.com>
2018-04-08 23:33:59 -0700

    Class added

--- sketches/gol.js
+++ sketches/gol.js
@@ -1,5 +1,11 @@
 
-resolution= 20
+function make2DArray (column,rows) {
+  let arr = new Array(column);
+  for (let i=0; i<arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
 
 function setup () {
   createCanvas(400, 400);
@@ -12,27 +18,30 @@ function draw () {
   grid.draw();
 }
 
-class grid {
-  constructor () {
-  column=width/resolution;
-  rows= height/resolution;
-  grid = make2DArray(column,rows);
-  for (let i=0; i<column; i++) {
-    for (let k=0; k < rows; k++) {
-      grid [i] [k] = floor(random(2));
+class Grid {
+  constructor (cellSize) {
+  this.cellSize= cellSize;
+
+  this.columns=width/cellSize;
+  this.rows= height/cellSize;
+
+  this.grid = make2DArray(this.columns,this.rows);
+  for (let i=0; i<this.columns; i++) {
+    for (let k=0; k < this.rows; k++) {
+      this.grid [i] [k] = floor(random(2));
   }
 }
   }
 
   draw () {
-  for (let i=0; i<column; i++) {
-    for (let k=0; k < rows; k++) {
-      let x=i * resolution;
-      let y= k * resolution;
-      if(grid[i][k]== 1) fill(0);
+  for (let i=0; i<this.columns; i++) {
+    for (let k=0; k < this.rows; k++) {
+      let x=i * this.cellSize;
+      let y= k * this.cellSize;
+      if(this.grid[i][k]== 1) fill(0);
       else fill (255);
       noStroke;
-      rect(x,y,resolution-1,resolution-1)
+      rect(x,y,this.cellSize-1,this.cellSize-1)
       }
     }
 }

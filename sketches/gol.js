77297eccfd8a26e14e407e4141303a44dc1fb5ca
fettycini <37485201+fettycini@users.noreply.github.com>
2018-04-21 15:02:27 -0700

    Update gol.js

--- sketches/gol.js
+++ sketches/gol.js
@@ -15,7 +15,8 @@ function draw() {
     background(250);
     grid.draw();
     grid.updateNeighborCounts();
-    cell.draw();
+    grid.updatePopulation
+    //cell.draw();
 
 }
 
@@ -28,20 +29,22 @@ class Cell {
     }
 
     liveOrDie() {
-    	
+
         var next = make2DArray(columns, rows);
 
-        for (var i = 0; i < columns; i++) {
-            for (var j = 0; j < rows; j++) {
-                var isAlive = grid[i][j];
+        for (var i = 0; i < this.columns; i++) {
+            for (var j = 0; j < this.rows; j++) {
+               this.isAlive = grid[i][j];
                 var neighbors = updateNeighborCounts(grid, i, j);
 
-                if (isAlive == 0 && neighbors == 3) {
+                if (this.isAlive == 0 && neighbors == 3) { //born on next gen.
                     next[i][j] = 1;
-                } else if (isAlive == 1 && (neighbors < 2 || neighbors > 3)) {
+
+                } else if (this.isAlive == 1 && (neighbors < 2 || neighbors > 3)) { //die on next gen.
                     next[i][j] = 0;
-                } else {
-                    next[i][j] = isAlive;
+
+                } else { //stay alive
+                    next[i][j] = this.isAlive;
                 }
 
             }
@@ -50,6 +53,11 @@ class Cell {
     }
 }
 
+// function keyPressed() {
+//     if (keyCode === ENTER) {
+//         if 
+//     }
+// }
 class Grid {
     constructor(cellSize) {
         this.cellSize = cellSize;
@@ -70,6 +78,8 @@ class Grid {
             for (var k = 0; k < this.rows; k++) {
                 var x = i * this.cellSize;
                 var y = k * this.cellSize;
+
+                //draw cell
                 if (this.grid[i][k] == 1) fill(0);
                 else fill(255);
                 noStroke;
@@ -84,13 +94,19 @@ class Grid {
                 var neighborX = this.columns + i
                 var neighborY = this.rows + j
 
-                print(neighborX + " ," + neighborY);
+                //print(neighborX + " ," + neighborY);
 
                 //sum += grid[x][y];
             }
         }
     }
     updatePopulation() {
+        population = 0;
 
+        for (var i = 0; i < this.columns; i++) {
+            for (var j = 0; j < this.rows; j++) {
+                if (grid[i][j]) population++;
+            }
+        }
     }
 }
